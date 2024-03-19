04fbce232f4fb056d2428d0c905a92d70416af59
Deep Vyas <deepvyas2003@gmail.com>
2024-03-19 11:20:05 +0530

    Fresh App

---
+++ appfrontend/screens/Projects/ProjectDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-svg'
+
+const ProjectDetails = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  )
+}
+
+export default ProjectDetails
